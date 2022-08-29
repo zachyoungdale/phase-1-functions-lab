d6a045cfd14122e6c557f53aca33b68e2e5f8d09
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-29 17:56:32 -0400

    commit tree 0bede582a6b1fd01130a49604c48195fb0195b98

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = someValue => {
+return Math.abs(someValue - 42);
+}
+
+const distanceFromHqInFeet = someValue => {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+const distanceTravelledInFeet = (start, destination) => {
+    let result = Math.abs((destination - start) * 264);
+    return result;
+}
+
+const calculatesFarePrice = (start, destination) => {
+    let result = Math.abs((destination - start) * 264);
+    if (result <= 400) {
+        return 0;
+    } else if (result > 400 && result <= 2000) {
+        return (result - 400) * 0.02;
+    } else if ( result > 2000 && result < 2500) {
+        return 25;
+    } else if (result >= 2500) {
+        return 'cannot travel that far';
+    }
+};
